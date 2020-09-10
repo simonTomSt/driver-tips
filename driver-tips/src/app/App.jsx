4f0b557f23d39267d170c0ae1ba2d592e40b4398
Szymon Stepien <st.szymon2001@gmail.com>
2020-09-10 19:31:58 +0200

    Add initial files

---
+++ driver-tips/src/app/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="App">wddddddddddd</div>;
+}
+
+export default App;
---
+++ driver-tips/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import App from "./app/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+serviceWorker.unregister();
